4d032c3174ee16be6e80e9194694ee1eb8ac4516
GroProGramer <1593675601@qq.com>
2016-08-12 20:44:09 +0800

    first commit

---
+++ app/src/main/java/com/firstonline/module/view/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.firstonline.module.view.activity;
+
+import android.os.Bundle;
+
+import com.firstonline.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
